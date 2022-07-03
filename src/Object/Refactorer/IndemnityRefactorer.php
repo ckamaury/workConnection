ece213703e9fca71d11fb57d649f7dd359dd1ae8
Romain FRANCOIS <romain.francois3@hotmail.fr>
2022-07-03 10:24:39 +0200

    1.0.14

---
+++ src/Object/Refactorer/IndemnityRefactorer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace CkAmaury\WorkConnection\Object\Refactorer;
+
+use CkAmaury\WorkConnection\Object\Refactorer\Object\Indemnity;
+
+class IndemnityRefactorer {
+
+    /** @return Indemnity[] */
+    public function refactor(array $data) : array{
+
+        $indemnities = array();
+        foreach($data as $item){
+            $indemnity = new Indemnity();
+            $indemnity
+                ->setAirportIata(ArrayUtils::get('stationCode',$item))
+                ->setAirportCity(ArrayUtils::get('stopover',$item))
+                ->setCurrency(ArrayUtils::get('localCurrency',$item))
+                ->setCurrencyRate(ArrayUtils::get('exchangeRate',$item))
+                ->setMealCost(ArrayUtils::get('mealCosts',$item))
+                ->setAllowanceCost(ArrayUtils::get('allowance',$item))
+                ->setAddText(ArrayUtils::get('additionalCompensation',$item) ?? '');
+            $indemnities[$indemnity->getAirportIata()] = $indemnity;
+        }
+
+        return $indemnities;
+
+    }
+
+}
---
+++ src/Object/Refactorer/Object/Indemnity.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace CkAmaury\WorkConnection\Object\Refactorer\Object;
+
+class Indemnity {
+
+    private string $airportIata;
+    private string $airportCity;
+    private float $currency;
+    private float $currencyRate;
+    private float $mealCost;
+    private float $allowanceCost;
+    private string $addText;
+
+    public function getAirportIata(): string {
+        return $this->airportIata;
+    }
+
+    public function setAirportIata(string $airportIata): self {
+        $this->airportIata = $airportIata;
+        return $this;
+    }
+
+    public function getAirportCity(): string {
+        return $this->airportCity;
+    }
+
+    public function setAirportCity(string $airportCity): self {
+        $this->airportCity = $airportCity;
+        return $this;
+    }
+
+    public function getCurrency(): float {
+        return $this->currency;
+    }
+
+    public function setCurrency(float $currency): self {
+        $this->currency = $currency;
+        return $this;
+    }
+
+    public function getCurrencyRate(): float {
+        return $this->currencyRate;
+    }
+
+    public function setCurrencyRate(float $currencyRate): self {
+        $this->currencyRate = $currencyRate;
+        return $this;
+    }
+
+    public function getMealCost(): float {
+        return $this->mealCost;
+    }
+
+    public function setMealCost(float $mealCost): self {
+        $this->mealCost = $mealCost;
+        return $this;
+    }
+
+    public function getAllowanceCost(): float {
+        return $this->allowanceCost;
+    }
+
+    public function setAllowanceCost(float $allowanceCost): self {
+        $this->allowanceCost = $allowanceCost;
+        return $this;
+    }
+
+    public function getAddText(): string {
+        return $this->addText;
+    }
+
+    public function setAddText(string $addText): self {
+        $this->addText = $addText;
+        return $this;
+    }
+
+
+
+}
